ea7804a8cd9ace91ae86354460eb8d303b44af88
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-11-26 11:55:02 -0300

    Exercicio 2

---
+++ bloco_4/dia_1/exer_2.js
@@ -0,0 +1,11 @@
+
+// 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+function retornaMaior (a,b) {
+  if (a > b) {
+    return a;
+  }else if (a < b) {
+    return b;
+  }
+  return 'São iguais';
+}
