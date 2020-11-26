843d106234b5e8b29906b6920ef94faf0e30620b
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-11-26 13:34:32 -0300

    Exercicio 10

--- bloco_4/dia_1/exer_10.js
+++ bloco_4/dia_1/exer_10.js
@@ -4,3 +4,23 @@
 // O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
 // valorCustoTotal = valorCusto + impostoSobreOCusto
 // lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+let v_custo = 10;
+let v_venda = 20;
+let quantidade = 1000;
+
+function lucroUnitario(preco_custo, preco_venda) {
+  let custo_total = preco_custo * 1.2;
+  return preco_venda - custo_total;
+}
+
+function lucroTotal(preco_custo, preco_venda, qnt) {
+  if (preco_custo <= 0 || preco_venda <= 0 || qnt <= 0){
+    return console.log('Valor ou quantidade digitada inválida.')
+  }
+  let l_unitario = lucroUnitario(preco_custo, preco_venda);
+  return l_unitario * qnt;
+}
+
+// console.log(`Lucro unitário: ${lucroUnitario(v_custo, v_venda)}`);
+console.log(`Lucro total: ${lucroTotal(v_custo, v_venda,quantidade)}`);
