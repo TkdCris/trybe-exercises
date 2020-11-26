cee357d5eedc80da1f4bfdd032e8713b62842098
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-11-26 02:03:36 -0300

    Removi arquivos comitados na branch errada

--- bloco_4/dia_1/exer_3.js
+++
@@ -1,8 +0,0 @@
-
-// 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
-let array = [39 ,30 ,12];
-function maiorValor(n) {
-  return Math.max.apply(null, array);
-}
-
-console.log(maiorValor(array));
