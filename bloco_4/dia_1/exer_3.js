ab6cae95255d2f344eaab273d51359b50f5a8964
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-11-26 11:57:06 -0300

    Exercicio 3

---
+++ bloco_4/dia_1/exer_3.js
@@ -0,0 +1,8 @@
+
+// 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+let array = [39 ,30 ,12];
+function maiorValor(n) {
+  return Math.max.apply(null, array);
+}
+
+console.log(maiorValor(array));
