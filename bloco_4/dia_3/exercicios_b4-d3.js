97af2e974603e29fc32b08a1993784b20886db8c
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-11-29 22:31:39 -0300

    Bloco_4 dia_3 exercicio 3

--- bloco_4/dia_3/exercicios_b4-d3.js
+++ bloco_4/dia_3/exercicios_b4-d3.js
@@ -12,11 +12,48 @@
 
 // 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 
-let tamanho = 3;
-let string = '';
-for (let i = 0; i < tamanho; i++) {
-  for (let j = -1; j < i; j++) {
-    string += '*';
+// let tamanho = 13;
+// let string = '';
+// for (let i = 0; i < tamanho; i++) {
+//   for (let j = -1; j < i; j++) {
+//     string += '*';
+//     j += i;
+//   }
+//   console.log(string);
+// }
+
+
+
+// 3- Agora inverta o lado do triângulo. Por exemplo:
+
+let colunas = 9;
+let linhas = Math.ceil(colunas/2);
+let cont = 1;
+
+for (i = 0; i < linhas; i++) {
+  let string = '';
+  for (j = 0; j < colunas; j++){
+    if (j < colunas - cont) {
+      string += ' ';
+    }else{
+      string += '*';
+    }
+
   }
+  if (i > 0) {
+    for (c = 0; c < i; c++) {
+      string += '*';
+    }
+  }
+  cont++;
   console.log(string);
 }
+
+
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+// n = 5
+
+//   *
+//  ***
+// *****
