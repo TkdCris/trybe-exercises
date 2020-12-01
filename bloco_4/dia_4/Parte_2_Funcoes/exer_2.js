d288601b9f96788dd8b5ab85bdd8b320a0e5c63d
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-01 01:04:30 -0300

    Bloco_4 dia_4 Bonus - Exercicio 2

--- bloco_4/dia_4/Parte_2_Funcoes/exer_2.js
+++ bloco_4/dia_4/Parte_2_Funcoes/exer_2.js
@@ -1,3 +1,18 @@
 // 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 // Array de teste: [2, 3, 6, 7, 10, 1]; .
 // Valor esperado no retorno da função: 4 .
+
+let numeros = [2, 3, 6, 7, 10, 1];
+
+function posicaoDoMaior(array) {
+  let maior = array[0];
+  let index = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (maior < array[i]) {
+      index = i;
+    }
+  }
+  return index;
+}
+
+console.log(posicaoDoMaior(numeros));
