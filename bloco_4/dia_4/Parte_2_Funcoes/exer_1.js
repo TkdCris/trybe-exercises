598c7dd562dcae6e24563b596de70c4a9c953224
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-01 00:54:55 -0300

    Bloco_4 dia_4 Bonus - Exercicio 1

--- bloco_4/dia_4/Parte_2_Funcoes/exer_1.js
+++ bloco_4/dia_4/Parte_2_Funcoes/exer_1.js
@@ -4,3 +4,19 @@
 // Retorno esperado: true
 // verificaPalindrome("desenvolvimento") ;
 // Retorno esperado: false
+
+
+function verificarPalindromo(string) {
+  let frase = string.toLowerCase().split('');
+  console.log(frase,' É um palíndromo? ');
+  let cont = frase.length - 1;
+  for (let i = 0; i < frase.length; i++) {
+    if (frase[i] != frase[cont]) {
+      return false;
+    }
+    cont--;
+  }
+  return true;
+}
+
+console.log(verificarPalindromo('mebaabem'));
