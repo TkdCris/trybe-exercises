18a1f571c592669308f2e9f1feac00053d63e087
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-01 01:07:58 -0300

    Bloco_4 dia_4 Bonus - Exercicio 3

--- bloco_4/dia_4/Parte_2_Funcoes/exer_3.js
+++ bloco_4/dia_4/Parte_2_Funcoes/exer_3.js
@@ -1,3 +1,18 @@
 // 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 // Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
 // Valor esperado no retorno da função: 6 .
+
+let numeros = [2, 4, 6, 7, 10, 0, -3];
+
+function posicaoDoMaior(array) {
+  let menor = array[0];
+  let index = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (menor > array[i]) {
+      index = i;
+    }
+  }
+  return index;
+}
+
+console.log(posicaoDoMaior(numeros));
