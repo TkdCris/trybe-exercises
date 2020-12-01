bbb8556ca9919fae41e3a51acbc9581123da22c7
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-01 01:16:05 -0300

    Bloco_4 dia_4 Bonus - Exercicio 4

--- bloco_4/dia_4/Parte_2_Funcoes/exer_4.js
+++ bloco_4/dia_4/Parte_2_Funcoes/exer_4.js
@@ -1,3 +1,19 @@
 // 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 // Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
 // Valor esperado no retorno da função: Fernanda .
+
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function maiorNome(array) {
+  let qnt = array[0].length;
+  let nome = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (qnt < array[i].length) {
+      qnt = array[i].length;
+      nome = array[i];
+    }
+  }
+  return nome;
+}
+
+console.log(maiorNome(nomes));
