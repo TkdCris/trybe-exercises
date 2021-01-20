75a5bfb67c737525c60c87924d7a06865f03bbcd
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-01-19 22:33:06 -0300

    Inicio bloco 8.2

--- bloco_8/dia_2/exercise1.js
+++ bloco_8/dia_2/exercise1.js
@@ -63,14 +63,26 @@ const books = [
   },
 ];
 
-// Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft'
+];
 
-const expectedResult = false;
+// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// Dica: Use a função map
 
-function authorUnique() {
+function formatedBookNames(books) {
   // escreva seu código aqui
-  return books.every((book, indexBook) => !books.some((book2, indexbook2) =>
-   book.author.birthYear === book2.author.birthYear && indexBook !== indexbook2));
+  let bookNames;
+  bookNames = books.map((book) => (`${book.name} - ${book.genre} - ${book.author.name}`));
+  console.log(bookNames)
+  return bookNames;
 }
 
-assert.strictEqual(authorUnique(), expectedResult);
+// formatedBookNames(books)
+
+assert.deepStrictEqual(formatedBookNames(), expectedResult);
